c145f4c8f587624b388654d0dd39a01cb8ce42e1
sgttux <116453812+sgttux@users.noreply.github.com>
2023-09-02 07:17:45 -0600

    Convert Stock to Contract Equiv
    Convert shares of stock to contract equivalent amount. Eg: 1200 shares would become a 12 contract equivalent amount to graph properly with calls and puts.

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-import opstrat as op
-
-op1={'op_type': 'c', 'strike': 215, 'tr_type': 's', 'op_pr': 7.63}
-op2={'op_type': 'c', 'strike': 220, 'tr_type': 'b', 'op_pr': 5.35}
-op3={'op_type': 'p', 'strike': 210, 'tr_type': 's', 'op_pr': 7.20}
-op4={'op_type': 's', 'strike': 205, 'tr_type': 'b', 'op_pr': 5.52}
-
-op_list=[op1, op2, op3, op4]
-op.multi_plotter(spot=212.26,spot_range=10, op_list=op_list)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
     README = readme_file.read()
 
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 DESCRIPTION = 'Option stategy visualizer'
 #LONG_DESCRIPTION = DESCRIPTION
 URL = 'https://github.com/hashabcd/opstrat'
